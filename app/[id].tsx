d1257acc93dd64b14ef2ab1b97a9c07659d98581
VitorLelis <vitorll909@gmail.com>
2024-08-19 23:50:17 +0100

    feat(info): options button

--- app/[id].tsx
+++ app/[id].tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import FontAwesome from '@expo/vector-icons/FontAwesome6';
 import { TouchableOpacity,StyleSheet } from "react-native";
 import { useDatabase } from "@/database/useDatabase";
+import { useActionSheet } from "@expo/react-native-action-sheet";
 
 export default function SwimmerInfo(){
  
@@ -14,7 +15,8 @@ export default function SwimmerInfo(){
     })
 
     const database = useDatabase();
-    const params = useLocalSearchParams<{id :string}>()
+    const params = useLocalSearchParams<{id :string}>();
+    const { showActionSheetWithOptions } = useActionSheet();
 
     useEffect(() => {
         if(params.id){
@@ -30,37 +32,67 @@ export default function SwimmerInfo(){
         }
     }, [params.id])
 
-    const handleEditPress = () => {
-        router.navigate({
-          pathname: '/editSwimmer',
-          params: {
-            id: params.id,
-            name: data.name,
-            gender: data.gender,
-            year_of_birth: data.year_of_birth.toString(),
+    const handleActionPress = (index: number) => {
+      switch (index) {
+          case 0:
+              // Navigate to Edit Swimmer screen
+              router.navigate({
+                  pathname: '/editSwimmer',
+                  params: {
+                      id: params.id,
+                      name: data.name,
+                      gender: data.gender,
+                      year_of_birth: data.year_of_birth.toString(),
+                  }
+              });
+              break;
+          case 1:
+              // Navigate to Add Time screen
+              /*router.navigate({
+                  pathname: '/addTime',
+                  params: { id: params.id }
+              });*/
+              break;
+          case 2:
+              // Handle Delete Swimmer action
+              // Implement your delete logic here
+              break;
+          default:
+              break;
+      }
+  };
+
+  const showActionSheet = () => {
+      const options = ['Edit Swimmer', 'Add Time', 'Delete Swimmer', 'Cancel'];
+      const cancelButtonIndex = 3;
+
+      showActionSheetWithOptions(
+          {
+              options,
+              cancelButtonIndex,
+              destructiveButtonIndex: 2,
+          },
+          (buttonIndex: number | undefined) => {
+            if (buttonIndex !== undefined) {
+              handleActionPress(buttonIndex);
+            }
           }
-        });
-      };
+      );
+  };
     
     return(
-        <View>
+        <View style={{flex: 1}}>
             <Text style={{fontSize: 20,fontWeight: 'bold',}}> INFO</Text>
             <Text> Name: {data.name}</Text>
             <Text> Gender: {data.gender}</Text>
             <Text> Year of Birth: {data.year_of_birth}</Text>
             
-            <TouchableOpacity style={styles.button} onPress={handleEditPress}>
-              <Text style={styles.buttonText}>Edit Info</Text>
-              <FontAwesome name="edit" color="#fff"/>
-            </TouchableOpacity>
-            
             <Text style={{fontSize: 20,fontWeight: 'bold',}}> TIMES</Text>
             <Text>TODO: Picker event</Text>
             <Text>TODO: Time Flatlist</Text>
             
-            <TouchableOpacity style={styles.button}>
-              <Text style={styles.buttonText}>Delete Swimmer</Text>
-              <FontAwesome name="trash-can" color="#fff"/>
+            <TouchableOpacity style={styles.fab} onPress={showActionSheet}>
+              <FontAwesome name="ellipsis-vertical" color="#fff" size={24} />
             </TouchableOpacity>
         </View>
     )
@@ -78,5 +110,17 @@ const styles = StyleSheet.create({
       color: '#fff',
       marginRight: 8,
       fontSize: 16,
+    },
+    fab: {
+      position: "absolute",
+      right: 20,
+      bottom: 20,
+      backgroundColor: "#007AFF",
+      borderRadius: 30,
+      width: 60,
+      height: 60,
+      justifyContent: "center",
+      alignItems: "center",
+      elevation: 5,
     },
   });
