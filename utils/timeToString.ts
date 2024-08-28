a939bba746d4e2574e8524be220430e74b1c813c
VitorLelis <vitorll909@gmail.com>
2024-08-28 23:17:08 +0100

    fix(timeToString): seconds format correction

--- utils/timeToString.ts
+++ utils/timeToString.ts
@@ -1,8 +1,10 @@
 export default function timeToString(time: number) : string{
+  let result: string = ''
     if (time >= 60){
         const minutes = Math.floor(time / 60);
         const seconds = time % 60;
-        return `${minutes}:${seconds.toFixed(2).toString()}`;
+        if (seconds>=10){ return `${minutes}:${seconds.toFixed(2).toString()}`}
+        else{ return `${minutes}:0${seconds.toFixed(2).toString()}`}
       }else{
         return `${time.toFixed(2).toString()}`;
       }
